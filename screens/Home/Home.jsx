5d325808f20ecd0e7d0d3749ff44045c3fe0e594
CodehubNerd <111436414+CodehubNerd@users.noreply.github.com>
2023-03-30 02:11:08 +0200

    added search UI and other components

--- screens/Home/Home.jsx
+++ screens/Home/Home.jsx
@@ -1,14 +1,38 @@
-import { View, Text,Image ,SafeAreaView,TouchableOpacity} from 'react-native'
-import React from 'react'
-import { welcomebg } from "../../constants";
+import { View, Text, SafeAreaView, TextInput, Image, TouchableOpacity } from 'react-native'
+import { useNavigation} from '@react-navigation/native';
+import React, { useState } from 'react'
+import { SimpleLineIcons ,Feather  } from '@expo/vector-icons'; 
 import styles from "./home.style";
 
 
 const Home = () => {
+  const back = useNavigation();
   return (
-    <View>
-      <Text>Home</Text>
+    
+    <SafeAreaView style={[styles.homecontainer, { flex: 1 }]} >
+    <View style={styles.header}>
+      <TouchableOpacity onPress={() => { }}>
+        <SimpleLineIcons style={{ backgroundColor: '#dee0e4b9', padding: 4, borderRadius: 4 }} name="menu" size={22} color="#091120b9" />
+      </TouchableOpacity>
+      <View style={{ flex: 1 }} />
+      <Image source={{ uri: 'https://res.cloudinary.com/dbb4s7ej0/image/upload/v1666080054/AdminPhotos/20220903_112222_gq40jt.jpg' }} style={styles.image} />
+      <View style={{ width: 20 }} />
     </View>
+    <View style={styles.userSection}>
+      <Text style={styles.welcomeText}>Hello, Masiko</Text>
+        <Text style={styles.createMemoriesText}>Create your memories</Text>
+        
+      <View style={styles.searchBoxContainer}>
+      <TextInput style={styles.searchBox} placeholder="Looking for?" />
+      <TouchableOpacity style={styles.searchButton}>
+        <Feather name="search" size={23} color="#fff" />
+      </TouchableOpacity>
+        </View>
+        
+
+    </View>
+  </SafeAreaView>
+  
   )
 }
 
--- screens/Home/home.style.js
+++ screens/Home/home.style.js
@@ -1,89 +1,105 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native'
 import { COLORS, FONT, SIZES } from "../../constants";
 
-
 const styles = StyleSheet.create({
-    container: {
-      flex:1,
-      alignItems: "center",
-      justifyContent: "center",
-      backgroundColor: "#f5f5f5",
-      position: "relative",
-  },
-  smallheading:{
-    fontSize: SIZES.regular,
-    fontFamily: FONT.medium,
-    color: COLORS.primary,
-  },
-  Bigheading:{
-    fontSize: SIZES.xxLarge,
-    marginTop:10,
-    fontFamily: FONT.bold,
-    color: COLORS.primary,
-  },
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop:50,
+    alignItems: 'center',
   
-  btn:{
-    backgroundColor: COLORS.primary,
-    padding: 15,
+  
+  },
+  homecontainer: {
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+  },
+  guest: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#000'
+  },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingHorizontal: 20
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    marginBottom: 20
+    },
+    imageContainer: {
+        alignItems: 'center',
+        marginVertical: 20,
+      },
+      image: {
+        width: 35,
+        height: 35,
+        borderRadius:80,
+      },
+  input: {
+    height: 40,
+    width: '100%',
+    marginVertical: 10,
     borderRadius: 5,
-    width: "100%",
-    alignItems: "center",
-    shadowColor: "#a3008b",
-    shadowOffset: {
-      width: 0,
-      height: 0,
-    },
-    shadowOpacity: 0.8,
-    shadowRadius: 10,
-    elevation: 10,
+    paddingHorizontal: 10,
+    borderWidth: 1,
+    borderColor: '#ccc'
   },
-  
-    imagestyle: {
-      width: "100%",
-      height: "100%",
-      object:"cover"
+  buttonContainer: {
+    backgroundColor:COLORS.primary,
+
+    borderRadius: 5,
+    padding: 10,
+    marginTop: 20
   },
-    
-    areaview: {
-      width: "80%",
-      height: "80%",
-    },
-    jobTitleBox: {
-      marginTop: SIZES.small,
-    },
-    jobTitle: {
-      fontSize: SIZES.large,
-      color: COLORS.primary,
-      fontFamily: FONT.bold,
-      textAlign: "center",
-    },
-    companyInfoBox: {
-      marginTop: SIZES.small / 2,
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    companyName: {
-      fontSize: SIZES.medium - 2,
-      color: COLORS.primary,
-      fontFamily: FONT.medium,
-    },
-    locationBox: {
-      flexDirection: "row",
-      justifyContent: "center",
-      alignItems: "center",
-    },
-    locationImage: {
-      width: 14,
-      height: 14,
-      tintColor: COLORS.gray,
-    },
-    locationName: {
-      fontSize: SIZES.medium - 2,
-      color: COLORS.gray,
-      fontFamily: FONT.regular,
-      marginLeft: 2,
-    },
-  });
-  
-  export default styles;
+  button: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#fff'
+  },
+  userSection: {
+    marginTop:25
+  },
+  welcomeText: {
+    fontSize: SIZES.xxLarge,
+    fontFamily: FONT.bold,
+    color: COLORS.tertiary,
+  },
+  createMemoriesText: {
+    fontSize: SIZES.regular,
+    marginTop:8,
+    fontFamily: FONT.regular,
+    color: COLORS.tertiary,
+  },
+  searchBoxContainer: {
+    flexDirection: 'row',
+    marginTop:10,
+    alignItems: 'center',
+  },
+  searchButton: {
+    backgroundColor: COLORS.primary,
+    borderRadius: 5,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginLeft: 10,
+  },
+
+
+  searchBox: {
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    fontSize: 16,
+    borderColor: '#ddd',
+    borderWidth: 1,
+    width: '80%',
+    flexGrow: 1,
+   
+  },
+})
+
+export default styles
