97cead3c17fdcf384d76524e709f11003373dfef
CodehubNerd <111436414+CodehubNerd@users.noreply.github.com>
2023-04-02 00:10:15 +0200

    adding misonary lay out componet

--- componets/index.js
+++ componets/index.js
@@ -2,11 +2,13 @@ import ScreenHeaderBtn from "./ScreenHeaderBtn/ScreenHeaderBtn";
 import Bottomnavigation from "./Bottomnavigation/Bottomnavigation";
 import Sidemenu from "./Sidemenu/Sidemenu";
 import CategoryList from "./Categories/CategoryList";
+import MasionaryLayout from "./MasionaryLayout/MasionaryLayout";
 
 
 export {
     ScreenHeaderBtn,
     Bottomnavigation,
     Sidemenu,
-    CategoryList
+    CategoryList,
+    MasionaryLayout
 };
--- screens/Home/Home.jsx
+++ screens/Home/Home.jsx
@@ -2,7 +2,7 @@ import { View, Text, SafeAreaView, TextInput, Image,ScrollView, TouchableOpacity
 import { useNavigation } from '@react-navigation/native';
 import MasonryList from '@react-native-seoul/masonry-list';
 import React, { useState } from 'react' 
-import {Bottomnavigation,Sidemenu,CategoryList} from '../../componets'
+import {Bottomnavigation,Sidemenu,CategoryList,MasionaryLayout} from '../../componets'
 import { SimpleLineIcons ,Feather  } from '@expo/vector-icons'; 
 import styles from "./home.style";
 
@@ -12,25 +12,18 @@ const Home = () => {
   const [showMenu, setShowMenu] = useState(false); //add state to control the visibility of the menu
   const [snaps, setSnaps] = useState(null);
 
-  const data = [
-    {id:1,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2023/03/25/17/35/girl-7876505_960_720.jpg'},
-    {id:2,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2023/03/27/11/17/wild-plant-7880509_960_720.jpg'},
-    {id:3,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2017/08/06/20/11/wedding-2595862_960_720.jpg'},
-    {id:4,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2014/02/07/11/31/flowers-260894_960_720.jpg'},
-    {id:5,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2016/11/21/16/08/tying-hair-1846171_960_720.jpg'},
-    {id:6,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_960_720.jpg'},
-    {id:7,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2016/10/07/08/56/close-up-1721060_960_720.jpg'},
-    {id:8,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2019/09/24/06/10/insect-4500348_960_720.jpg'},
-    {id:9,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2019/12/15/08/14/body-painting-4696539_960_720.jpg'},
-  ]
+
 
   const handleMenuToggle = () => {
-    setShowMenu(!showMenu); //toggle the value of showMenu when the user clicks the SimpleLineIcons button
+    setShowMenu(!showMenu); 
   };
 
   return (
-    
+
+    //home screen container
     <SafeAreaView style={[styles.homecontainer, { flex: 1 }]} >
+      
+
     <View style={styles.header}>
     <TouchableOpacity onPress={handleMenuToggle}>
         <SimpleLineIcons style={{ backgroundColor: '#dee0e4b9', padding: 4, borderRadius: 4 }} name="menu" size={22} color="#091120b9" />
@@ -49,31 +42,18 @@ const Home = () => {
       <TouchableOpacity style={styles.searchButton}>
       <Feather name="search" size={23} color="#fff" />
       </TouchableOpacity>
-        </View>
+      </View>
+
+        {/*Flatlist categories component*/}
         <CategoryList />
+
+
       </View>
      
      
-     
-
-      <ScrollView style ={{padding:4}} >
-        {data ? (
-          <>
-          <MasonryList
-          data={data}
-          keyExtractor={(item)=> item.id}
-          numColumns={2}
-          showsVerticalScrollIndicator={false}
-          renderItem={({ item }) => <CardItem data={item} />}
-          />
-          </>) : (
-          <>
-          <ActivityIndicator size={30} color={'#00A6A6'}/>
-        </>
-       )}
-    
-        
-      </ScrollView>
+      {/*srcoll view of (misonary) images component*/}
+      
+      <MasionaryLayout/>
 
       
  {/* show/hide the SideMenu component based on the value of showMenu */}
@@ -87,25 +67,7 @@ const Home = () => {
   )
 }
 
-const CardItem = ({data}) => {
-  return (
-    <TouchableOpacity  style={{
-      margin: 1,
-      margin: 6,
-      borderRadius: 5, // Equivalent to rounded-md in Tailwind
-      position: 'relative',
-      overflow: 'hidden',
-      height: Math.round(Math.random() * 100 + 200),
-    }}
-    >
-     <Image source ={{uri : data.imageURL}}   style={{
-    width: '100%',
-    height: '100%',
-    resizeMode: 'cover',
-  }}  />  
-  </TouchableOpacity>
-)
-}
+
 
 
 export default Home;
