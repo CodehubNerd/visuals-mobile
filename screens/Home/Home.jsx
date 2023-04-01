a5269689080488a02ae1bc6484af1c3f0969eae7
CodehubNerd <111436414+CodehubNerd@users.noreply.github.com>
2023-04-01 12:51:25 +0200

    images

--- screens/Home/Home.jsx
+++ screens/Home/Home.jsx
@@ -12,6 +12,18 @@ const Home = () => {
   const [showMenu, setShowMenu] = useState(false); //add state to control the visibility of the menu
   const [snaps, setSnaps] = useState(null);
 
+  const data = [
+    {id:1,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2023/03/25/17/35/girl-7876505_960_720.jpg'},
+    {id:2,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2023/03/27/11/17/wild-plant-7880509_960_720.jpg'},
+    {id:3,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2017/08/06/20/11/wedding-2595862_960_720.jpg'},
+    {id:4,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2014/02/07/11/31/flowers-260894_960_720.jpg'},
+    {id:5,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2016/11/21/16/08/tying-hair-1846171_960_720.jpg'},
+    {id:6,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2013/07/18/20/26/sea-164989_960_720.jpg'},
+    {id:7,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2016/10/07/08/56/close-up-1721060_960_720.jpg'},
+    {id:8,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2019/09/24/06/10/insect-4500348_960_720.jpg'},
+    {id:9,name : 'Reading', imageURL : 'https://cdn.pixabay.com/photo/2019/12/15/08/14/body-painting-4696539_960_720.jpg'},
+  ]
+
   const handleMenuToggle = () => {
     setShowMenu(!showMenu); //toggle the value of showMenu when the user clicks the SimpleLineIcons button
   };
@@ -45,7 +57,7 @@ const Home = () => {
 
       <ScrollView>
         {snaps ? (
-          <></>) :( <>
+         <> </>) :( <>
           <ActivityIndicator size={30} color={'#00A6A6'}/>
         </>
        )}
