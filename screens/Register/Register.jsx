ba62b729d78373d83ee0e7d52b61553a4d0d5afc
CodehubNerd <111436414+CodehubNerd@users.noreply.github.com>
2023-04-03 23:26:41 +0200

    image - not fixed

--- screens/Register/Register.jsx
+++ screens/Register/Register.jsx
@@ -4,18 +4,18 @@ import { client } from '../../sanity'
 import * as ImagePicker from 'expo-image-picker';
 
 import { useNavigation } from '@react-navigation/native';
+import URL from 'react-native-url-polyfill';
+
 import { AntDesign } from '@expo/vector-icons';
 import styles from "./register.style";
 
 const Register = () => {
   const [email, setEmail] = useState('');
-  const [username, setusername] = useState('');
+  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [image, setImage] = useState(null);
 
-  //handle image upload
-
-
+  // handle image upload
   const handleImagePicker = async () => {
     const result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.Images,
@@ -28,12 +28,19 @@ const Register = () => {
     }
   };
 
-  const naviagation = useNavigation();
+  const navigation = useNavigation();
+
+  const handleRegister = async () => {
+    const newUser = { email, username, password, imageUrl: image };
+    const result = await client.create({ _type: 'user', ...newUser });
+    navigation.navigate('RegistrationSuccess');
+  };
+
 
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={styles.header}>
-        <TouchableOpacity onPress={() => {back.navigate("Login")}}>
+        <TouchableOpacity onPress={() => navigation.goBack()}>
           <AntDesign name="back" size={24} color="#091120" />
         </TouchableOpacity>
         <View style={{ flex: 1 }} />
@@ -42,16 +49,16 @@ const Register = () => {
       </View>
       <View style={styles.container}>
         <Text style={styles.title}>Create account</Text>
-        <TouchableOpacity style={styles.imagePickerButton}  onPress={handleImagePicker}>
-          {image ?
-            <Image source={{ uri: image }} style={styles.imagePickerImage}/>
-            :
+        <TouchableOpacity style={styles.imagePickerButton} onPress={handleImagePicker}>
+          {image ? (
+            <Image source={{ uri: image }} style={styles.imagePickerImage} />
+          ) : (
             <View style={styles.imagePlaceholder}>
-            <View style={styles.imageWrapper}>
-             <AntDesign name="camerao" size={48} color="#7a7a7a" style={styles.imagePickerIcon} />
+              <View style={styles.imageWrapper}>
+                <AntDesign name="camerao" size={48} color="#7a7a7a" style={styles.imagePickerIcon} />
+              </View>
             </View>
-           </View>
-          }
+          )}
         </TouchableOpacity>
         <TextInput
           style={styles.input}
@@ -66,7 +73,7 @@ const Register = () => {
           placeholder="Username"
           autoCapitalize="none"
           value={username}
-          onChangeText={setusername}
+          onChangeText={setUsername}
         />
         <TextInput
           style={styles.input}
@@ -76,16 +83,18 @@ const Register = () => {
           value={password}
           onChangeText={setPassword}
         />
-        <TouchableOpacity style={[styles.buttonContainer, { width: '100%' }]} onPress={() => {}}>
-          <Text style={[styles.button, { textAlign: 'center' }]}>Register</Text>
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => {naviagation.navigate("Login")}}>
-          <Text style={{marginTop:6}}>Already have an account</Text>
+     <TouchableOpacity
+      style={[styles.buttonContainer, { width: '100%' }]}
+      onPress={handleRegister}
+    >
+      <Text style={[styles.button, { textAlign: 'center' }]}>Register</Text>
+    </TouchableOpacity>
+        <TouchableOpacity onPress={() => navigation.navigate('Login')}>
+          <Text style={{ marginTop: 6 }}>Already have an account</Text>
         </TouchableOpacity>
       </View>
     </SafeAreaView>
-  )
-}
-
+  );
+};
 
 export default Register;
