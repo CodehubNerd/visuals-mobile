69c7868592aa6dd3d58b2df7e3ee4173e51a2f8f
CodehubNerd <111436414+CodehubNerd@users.noreply.github.com>
2023-04-18 02:44:16 +0200

    updates  sanity

---
+++ sanity.js
@@ -0,0 +1,16 @@
+import { createClient } from "@sanity/client"
+import imageUrlBuilder from "@sanity/image-url"
+
+const client = createClient({
+    projectId: 'rrk2vc8o',
+    dataset: 'production',
+    useCdn: true,
+    apiVersion: '2023-04-18'
+})
+
+const builder = imageUrlBuilder(client)
+export const urlFor = (source) => builder.image(source);
+
+export const getCategory = async () => {
+    const items = await client.fetch('*[_type == ]')
+}
