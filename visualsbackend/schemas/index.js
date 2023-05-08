a362ae221c83dd9e8deb0a6aaa35aa104293a171
CodehubNerd <111436414+CodehubNerd@users.noreply.github.com>
2023-05-08 11:36:13 +0200

    added 2 schemas

--- visualsbackend/schemas/index.js
+++ visualsbackend/schemas/index.js
@@ -2,6 +2,8 @@
 import items from './items'
 import category from './category'
 import users from './users'
+import save from './save'
+import postedBy from './postedBy'
 
 
-export const schemaTypes = [items,category,users ]
+export const schemaTypes = [items,category,users,save,postedBy]
